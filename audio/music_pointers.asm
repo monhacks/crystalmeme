1b7e756f3c096cd0d90dd7418cc825eecdfd30c2
Larson T. (Lrs121) <ltthorpe@lrsservers.org>
2025-06-19 10:04:56 -0700

    comment out unused music

--- audio/music_pointers.asm
+++ audio/music_pointers.asm
@@ -109,8 +109,7 @@ Music:
 	dba Music_Shop
 	dba Music_Intermission
 	dba Music_CinnabarMansion
-	dba Music_BlackCity
-	dba Music_DewfordTown
-	dba Music_EternaForest
-	dba Music_StarkMountain
-
+;	dba Music_BlackCity
+;	dba Music_DewfordTown
+;	dba Music_EternaForest
+;	dba Music_StarkMountain
